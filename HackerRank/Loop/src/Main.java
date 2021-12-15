609c86a54d84f92f4e14817e61c38f5ddd9309c6
ivanadnin <ivandanin01@gmail.com>
2021-12-15 11:58:16 +0200

    loop

---
+++ HackerRank/Loop/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int q = Integer.parseInt(scanner.nextLine());
+
+
+        for (int i = 0; i < q; i++) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            int n = scanner.nextInt();
+
+            int sum = a;
+
+            for (int j = 0; j <= n - 1; j++) {
+                sum += Math.pow(2, j) * b;
+                System.out.println(sum);
+            }
+        }
+
+    }
+}
