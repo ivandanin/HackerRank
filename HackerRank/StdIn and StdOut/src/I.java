eaccd0f059514319a2b8421b998938c922a199a5
ivanadnin <ivandanin01@gmail.com>
2021-12-21 16:55:40 +0200

    stdin and stdout

---
+++ HackerRank/StdIn and StdOut/src/I.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class I {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int integer1 = scanner.nextInt();
+        int integer2 = scanner.nextInt();
+        int integer3 = scanner.nextInt();
+
+        System.out.println(integer1);
+        System.out.println(integer2);
+        System.out.println(integer3);
+    }
+}
---
+++ HackerRank/StdIn and StdOut/src/II.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class II {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int integer = Integer.parseInt(scanner.nextLine());
+        double num = Double.parseDouble(scanner.nextLine());
+        String string = scanner.nextLine();
+
+        System.out.println("String: " + string);
+        System.out.println("Double: " + num);
+        System.out.println("Int: " + integer);
+    }
+}
+o
