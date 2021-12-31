02dc65a7f55a23a7afee4f51d2360069a16d10f6
ivandanin <ivandanin01@gmail.com>
2021-12-31 11:57:41 +0200

    syntax checker

--- HackerRank/Pattern Syntax Checker/src/Main.java
+++ HackerRank/Pattern Syntax Checker/src/Main.java
@@ -1,2 +1,27 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        while (n-- > 0) {
+            PatternSyntaxException exception = null;
+            String toCheck = scanner.nextLine();
+
+            try {
+            Pattern.compile(toCheck);
+            } catch (PatternSyntaxException e) {
+                exception = e;
+            }
+            if (exception != null) {
+                System.out.println("Invalid");
+            } else {
+                System.out.println("Valid");
+            }
+        }
+    }
 }
