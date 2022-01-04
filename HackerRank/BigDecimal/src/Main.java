58231ca9239d8c18f858c9471233f743edc823bb
ivanadnin <ivandanin01@gmail.com>
2022-01-04 20:18:53 +0200

    bigdecimal

--- HackerRank/BigDecimal/src/Main.java
+++ HackerRank/BigDecimal/src/Main.java
@@ -1,24 +1,37 @@
 import java.math.BigDecimal;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        List<BigDecimal> list = new LinkedList<>();
-
         int n = Integer.parseInt(scanner.nextLine());
 
-        while (n-- > 0) {
-            String s = scanner.nextLine();
-
-            BigDecimal num = new BigDecimal(s);
-            list.add(num);
+        String[] s = new String[n + 2];
+        for (int i = 0; i < n; i++) {
+            s[i] = scanner.next();
         }
-        list.stream()
-                .sorted(Comparator.reverseOrder())
-                .collect(Collectors.toList())
-                .forEach(System.out::println);
+
+        for (int i = 0; i < n; i++) {
+            BigDecimal max = new BigDecimal(s[i]);
+            int index = i;
+
+            for (int j = i + 1; j < n; j++) {
+                BigDecimal curr = new BigDecimal(s[j]);
+
+                if (curr.compareTo(max) > 0) {
+                    max = curr;
+                    index = j;
+                }
+            }
+                    String temp = s[i];
+                    s[i] = s[index];
+                    s[index] = temp;
+
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(s[i]);
+        }
     }
 }
