b6764e9c3a6944c3e3ceca968ddbcd10b696d929
ivanadnin <ivandanin01@gmail.com>
2022-01-04 21:33:03 +0200

    subarray

---
+++ HackerRank/Subarray/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j < n; j++) {
+                sum += arr[j];
+
+                if (sum < 0) {
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
