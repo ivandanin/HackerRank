d7f0640a54ebe941435be7fa42311944fb7b8b48
ivanadnin <ivandanin01@gmail.com>
2022-01-11 14:16:47 +0200

    instanceOf

---
+++ HackerRank/InstanceOf/src/Main.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Student {}
+class Rockstar {}
+class Hacker {}
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Object> arrayList = new ArrayList<>();
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        while (n-- > 0) {
+            String person = scanner.nextLine();
+            switch (person) {
+                case "Student" -> arrayList.add(new Student());
+                case "Rockstar" -> arrayList.add(new Rockstar());
+                case "Hacker" -> arrayList.add(new Hacker());
+            }
+        }
+            count(arrayList);
+    }
+    static void count(ArrayList<Object> arrayList) {
+        int stud = 0;
+        int rock = 0;
+        int hacker = 0;z
+
+        for (Object o : arrayList) {
+            if (o instanceof Student) {
+                stud++;
+            } else if (o instanceof Rockstar) {
+                rock++;
+            } else if (o instanceof Hacker) {
+                hacker++;
+            }
+        }
+        System.out.println(stud + " " + rock + " " + hacker);
+    }
+}
